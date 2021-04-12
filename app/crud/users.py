7ff7ab792577d01048bff90e926c5496af54b161
hanbin8269 <hanbin8269@gmail.com>
2021-04-12 14:02:23 +0900

    chore: Add context manage decorator

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,8 +1,10 @@
 from app.models import User
 from app.schemas import UserCreate
 from app.common.hashers import make_password
+from app.databases.session import db
 
 
+@db.db_connect
 def create_user(user: UserCreate):
     hashed_password = make_password(user.password)
     user_instance = User(email=user.email, password=hashed_password)
@@ -10,5 +12,6 @@ def create_user(user: UserCreate):
     return user_instance
 
 
+@db.db_connect
 def get_user_by_id(user_id: int):
     return User.filter(User.id == user_id).first()
