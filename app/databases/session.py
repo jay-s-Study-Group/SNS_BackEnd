2993b9f3e57344937de0568fc1ca9f311bc30365
hanbin8269 <hanbin8269@gmail.com>
2021-04-12 14:00:02 +0900

    feat: Create PeeweeDBConnection class

--- app/databases/session.py
+++ app/databases/session.py
@@ -1,10 +1,33 @@
 import peewee as pw
 from app.common.util import conf
+import functools
 
-conn = pw.MySQLDatabase(
-    "",
-    host=conf().DATABASE_HOST,
-    port=conf().DATABASE_PORT,
-    user=conf().DATABASE_USER,
-    passwd=conf().DATABASE_PASSWD,
-)
+
+class PeeweeDBConnection:
+    def __init__(self):
+        self._session = pw.MySQLDatabase(
+            database=conf().DATABASE_NAME,
+            host=conf().DATABASE_HOST,
+            port=conf().DATABASE_PORT,
+            user=conf().DATABASE_USER,
+            passwd=conf().DATABASE_PASSWD,
+        )
+
+    @property
+    def session(self):
+        return self._session
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self._session.is_closed():
+            self._session.close()
+
+    def db_connect(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with self._session.connection_context():
+                return func(*args, **kwargs)
+
+        return wrapper
+
+
+db = PeeweeDBConnection()
