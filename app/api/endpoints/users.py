c28982e4e955a7c29aa7a3744c73ab97c0b77c83
hanbin8269 <hanbin8269@gmail.com>
2021-04-12 14:02:35 +0900

    feat: Create creat_user api

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.schemas import User
-from app.crud import get_user_by_id
+from app.schemas import User, UserCreate
+from app.crud import get_user_by_id, create_user
 
 router = APIRouter()
 
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get("/{user_id}", response_model=User)
 def get_user(user_id: int):
     return get_user_by_id(user_id=user_id)
+
+
+@router.post("/", response_model=User)
+def create_user(user: UserCreate):
+    return create_user(**user)
