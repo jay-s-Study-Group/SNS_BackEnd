e7971280fb07cce55536c1169ad739eeb6e8c5bd
hanbin8269 <hanbin8269@gmail.com>
2021-04-20 22:51:00 +0900

    chore: Rename url

--- app/api/endpoints/social_auth.py
+++ app/api/endpoints/social_auth.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.utils.authentication import KakaoSocialAuthentication
+from app.utils.authentication import (
+    get_kakao_access_token,
+)
 from app.utils.config import load_config
 
 SETTINGS = load_config()
@@ -7,17 +9,16 @@ SETTINGS = load_config()
 router = APIRouter()
 
 
-@router.get("/login")
+@router.get("/kakao/login_url")
 def get_social_oauth_link_list():
     return {
-        "kakao_login": "https://kauth.kakao.com/oauth/authorize?client_id={0}&response_type=code&redirect_uri={1}".format(
+        "login_url": "https://kauth.kakao.com/oauth/authorize?client_id={0}&response_type=code&redirect_uri={1}".format(
             SETTINGS.KAKAO_API_CLIENT_ID, SETTINGS.KAKAO_OAUTH_REDIRECT_URI
         )
     }
 
 
-@router.get("/kakao-login")
+@router.get("/kakao/access-token")
 def kakao_oauth_redirect(code: str):  # TODO: kakao login pydantic form 추가
-    authentication = KakaoSocialAuthentication()
-    access_token = authentication.get_access_token(code)
+    access_token = get_kakao_access_token(code)
     return {"access_token": access_token}
