3ebe6bbc057657c580d5e580f04b1f154398a4d5
hanbin8269 <hanbin8269@gmail.com>
2021-04-20 21:12:15 +0900

    chore: Delete KakaoSocialAuthentication class

--- app/utils/authentication.py
+++ app/utils/authentication.py
@@ -4,26 +4,24 @@ from app.utils.config import load_config
 CONFIG = load_config()
 
 
-class BaseSocialAuthentication:
-    def get_access_token(self, code):
-        raise NotImplementedError()  # TODO: Add  error message
+def get_kakao_access_token(code):
+    data = {
+        "grant_type": "authorization_code",
+        "client_id": CONFIG.KAKAO_API_CLIENT_ID,
+        "redirect_uri": CONFIG.KAKAO_OAUTH_REDIRECT_URI,
+        "code": code,
+    }
+    response = requests.post("https://kauth.kakao.com/oauth/token", data=data)
+    print(response.status_code)
+    if response.status_code in (400, 401):
+        raise Exception(response.text)  # TODO: 직접 만든 exception 모듈로 교체
+    token_info = response.json()
+    access_token = token_info["access_token"]
+    return access_token
 
 
-class KakaoSocialAuthentication(BaseSocialAuthentication):
-    def __init__(self):
-        self.redirect_uri = CONFIG.KAKAO_OAUTH_REDIRECT_URI
+def get_kakao_user_info(access_token):
+    headers = {"Authorization": "Bearer " + access_token}
+    response = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
 
-    def get_access_token(self, code):
-        data = {
-            "grant_type": "authorization_code",
-            "client_id": CONFIG.KAKAO_API_CLIENT_ID,
-            "redirect_uri": self.redirect_uri,
-            "code": code,
-        }
-        response = requests.post("https://kauth.kakao.com/oauth/token", data=data)
-        print(response.status_code)
-        if response.status_code in (400, 401):
-            raise Exception(response.text)  # TODO: 직접 만든 exception 모듈로 교체
-        token_info = response.json()
-        access_token = token_info["access_token"]
-        return access_token
+    return response.json()
