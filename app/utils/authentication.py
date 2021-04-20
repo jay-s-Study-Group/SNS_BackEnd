3236356b11c2b5d39afb68f54a67bbcd9f7f40a7
hanbin8269 <hanbin8269@gmail.com>
2021-04-20 21:13:38 +0900

    feat: JWTAuthentication class 생성

--- app/utils/authentication.py
+++ app/utils/authentication.py
@@ -1,7 +1,39 @@
 import requests
+import jwt
 from app.utils.config import load_config
 
 CONFIG = load_config()
+
+
+def jwt_encode_handler(payload):
+    key = CONFIG.JWT_SECRET_KEY
+    algorithm = CONFIG.JWT_ALGORITHM
+
+    return jwt.encode(dict(payload), key, algorithm)
+
+
+def jwt_decode_handler(token):
+    key = CONFIG.JWT_SECRET_KEY
+    algorithm = CONFIG.JWT_ALGORITHM
+    return jwt.decode(token, key, algorithm)
+
+
+def jwt_payload_handler(user):
+    payload = {
+        "sub": user.email,
+        # "iss" : # TODO : 발급자 설정
+        "email": user.email,
+        # "exp": 1,  # TODO : 만료 시간 설정
+        # "iat": 1,  # TODO : 발급 시간 설정
+    }
+    return payload
+
+
+def get_kakao_user_info(access_token):
+    headers = {"Authorization": "Bearer " + access_token}
+    response = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
+
+    return response.json()
 
 
 def get_kakao_access_token(code):
@@ -20,8 +52,12 @@ def get_kakao_access_token(code):
     return access_token
 
 
-def get_kakao_user_info(access_token):
-    headers = {"Authorization": "Bearer " + access_token}
-    response = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
+class BaseAuthentication:
+    def authenticate(self, email, password):
+        raise NotImplementedError()  # TODO: Add  error message
 
-    return response.json()
+
+class JSONWebTokenAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        # TODO : token 검증 후 user 인스턴스 반환
+        return
