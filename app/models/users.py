d323d96b0e9651083ad3d34bf2fe4cfe79eb8c73
hanbin8269 <hanbin8269@gmail.com>
2021-04-20 23:02:51 +0900

    feat: SocialAuth, SocialPlatform 모델 추가

--- app/models/users.py
+++ app/models/users.py
@@ -8,3 +8,14 @@ class User(MySQLModel):
     email = pw.CharField(max_length=256, unique=True)
     password = pw.CharField(max_length=256)
     is_active = pw.BooleanField(default=False)
+
+
+class SocialPlatform(MySQLModel):
+    name = pw.CharField(max_length=50, primary_key=True)
+
+
+class SocialAuth(MySQLModel):
+    user = pw.ForeignKeyField(User, backref="social_auth_info")
+
+    platform = pw.ForeignKeyField(SocialPlatform, backref="social_auth_info")
+    sns_service_id = pw.CharField(max_length=100)
