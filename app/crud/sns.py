c08878842151b2f80686878e9b806bce7073ec67
hanbin8269 <hanbin8269@gmail.com>
2021-04-21 11:21:36 +0900

    feat: create_social_user DAO 추가

--- app/crud/sns.py
+++ app/crud/sns.py
@@ -1,4 +1,5 @@
 from app.models import User, SocialAuth
+from app.schemas import CreateSocialUserSchema
 from app.databases.session import db
 
 
@@ -14,9 +15,19 @@ def get_user_by_sns_service_id(sns_service_id: str):
 
 
 @db.db_connect
-def create_social_auth(user: User, platform: str, sns_service_id: str):
+def create_social_auth(user: User, sns_service_id: str, platform: str):
     social_auth_instance = SocialAuth(
         platform=platform, sns_service_id=sns_service_id, user=user
     )
     social_auth_instance.save()
     return social_auth_instance
+
+
+@db.db_connect
+def create_social_user(
+    user: CreateSocialUserSchema, sns_service_id: str, platform: str
+):
+    user_instance = User(
+        email=user.email, sns_service_id=sns_service_id, platform=platform
+    )
+    return user_instance
