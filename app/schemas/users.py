57af48f9ff007c68808a92315ebd777ccc4a3bfd
hanbin8269 <hanbin8269@gmail.com>
2021-04-21 11:22:01 +0900

    feat: 필요한 CreateSocialUserSchema, SNSServiceInfoSchema 스키마 추가

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -9,9 +9,17 @@ class CreateUserSchema(UserSchemaBase):
     password: str
 
 
+class CreateSocialUserSchema(UserSchemaBase):
+    pass  # 후에 전화번호, 성별 등 여러 정보 추가
+
+
 class GetUserSchema(UserSchemaBase):
     id: int
     is_active: bool
 
     class Config:
         orm_mode = True
+
+
+class SNSServiceInfoSchema(BaseModel):
+    sns_service_id: str
