c4558d4af44833c36c573163428ca7f4d195460c
hanbin8269 <hanbin8269@gmail.com>
2021-04-24 19:22:08 +0900

    chore: Response 폼 변경

--- app/api/endpoints/sns/kakao.py
+++ app/api/endpoints/sns/kakao.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, Header, Body
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, Header, Body, Response
 from fastapi.exceptions import HTTPException
 from typing import Any
 from app.utils.authentication import (
@@ -8,22 +7,23 @@ from app.utils.authentication import (
 from app.controllers.sns import KAKAOOAuthController
 from app.utils.config import load_config
 from app.models.users import User
+from app.schemas.users import GetUserSchema
 
 SETTINGS = load_config()
 
 router = APIRouter()
 
 
-@router.get("/kakao/login-url")
+@router.get("/kakao/login-url", status_code=200)
 def get_social_oauth_link_list():
     login_url = KAKAOOAuthController().get_login_url()
-    return JSONResponse(content={"login_url": login_url}, status_code=200)
+    return {"login_url": login_url}
 
 
-@router.get("/kakao/oauth-token")
+@router.get("/kakao/oauth-token", status_code=200)
 def kakao_oauth_redirect(code: str) -> Any:
     access_token = KAKAOOAuthController().get_oauth_token(code)
-    return JSONResponse(content={"oauth_token": access_token}, status_code=200)
+    return {"oauth_token": access_token}
 
 
 def get_current_user(authorization: str = Header(None)):
@@ -43,7 +43,7 @@ def validate_oauth_token(oauth_token: str = Body(..., embed=True)):
     return oauth_token
 
 
-@router.post("/kakao/connect")
+@router.post("/kakao/connect", status_code=201)
 def connect_user_to_kakao(
     user=Depends(get_current_user),
     oauth_token=Depends(validate_oauth_token),
@@ -51,17 +51,17 @@ def connect_user_to_kakao(
     social_auth_instance = KAKAOOAuthController().connect_social_login(
         oauth_token, user
     )
-    return JSONResponse(content=social_auth_instance.__data__, status_code=200)
+    return social_auth_instance.__data__
 
 
-@router.post("/kakao/register")
+@router.post("/kakao/register", response_model=GetUserSchema, status_code=201)
 def register_with_kakao(oauth_token=Depends(validate_oauth_token)):
     user_instance = KAKAOOAuthController().create_social_user(oauth_token)
-    return JSONResponse(content=user_instance.__data__, status_code=201)
+    return user_instance.__data__
 
 
-@router.post("/kakao/login")
-def login_with_kakao(oauth_token=Depends(validate_oauth_token)):
+@router.post("/kakao/login", response_model=GetUserSchema, status_code=200)
+def login_with_kakao(response: Response, oauth_token=Depends(validate_oauth_token)):
     user, token = KAKAOOAuthController().socical_login(oauth_token)
-    headers = {"Authorization": "jwt " + token}
-    return JSONResponse(content=user.__data__, headers=headers, status_code=200)
+    response.headers["Authorization"] = "jwt " + token
+    return user.__data__
