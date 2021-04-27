3658e8565c79181a8f89bd1f42ee2d1994eff627
hanbin8269 <hanbin8269@gmail.com>
2021-04-27 12:23:08 +0900

    chore: 불필요한 Response, Request form 제거

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -15,21 +15,9 @@ class CreateUserSchema(UserSchemaBase):
     password: str
 
 
-class CreateSocialUserSchema(UserSchemaBase):
-    pass  # 후에 전화번호, 성별 등 여러 정보 추가
-
-
 class GetUserSchema(UserSchemaBase):
     id: int
     is_active: bool
 
     class Config:
         orm_mode = True
-
-
-class SNSLoginSchema(BaseModel):
-    oauth_token: str
-
-
-class SNSServiceInfoSchema(BaseModel):
-    sns_service_id: str
