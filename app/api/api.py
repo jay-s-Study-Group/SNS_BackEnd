1011b62adc5d21d0f10b5e811a19e2e5963a7a2d
EunjaeYong <eunjae.yong@dslab.global>
2021-06-16 23:14:28 +0900

    멘토링 라우터 추가

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from app.api.endpoints import users
 from app.api.endpoints.sns import kakao
+from app.api.endpoints import mentors
 
 api_router = APIRouter()
 
 
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(kakao.router, prefix="/sns", tags=["sns"])
+api_router.include_router(mentors.router, prefix="/mentoring", tags=["mentoring"])
